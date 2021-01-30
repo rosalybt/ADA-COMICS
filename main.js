6d75edab823a088857b093a27e19aed724f82294
rosalybt <rosalybalbuena.15@gmail.com>
2021-01-30 19:44:00 -0400

    feature: funciones avance y retroceso doble(dos paginas)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let currentPage = 1
+let numberOfPages = 0
 
 
 
@@ -26,12 +27,13 @@ const getInfo = (url) => {
     fetch(url).then((data) => {
         return data.json();
     }).then((cover) => {
-        console.log(cover.results)
+
         const results = document.querySelector('.resultados')
         const containerCards = document.querySelector('.contenedor-cards')
         containerCards.innerHTML = ''
         results.innerHTML = ''
         results.innerHTML = cover.info.count
+        numberOfPages = cover.info.pages
         createCard(cover.results, containerCards);
     })
 };
@@ -41,6 +43,8 @@ getInfo('https://rickandmortyapi.com/api/character');
 
 const nextPage = document.querySelector('#btn-next')
 const previousPage = document.querySelector('#btn-previous')
+const doubleNextPage = document.querySelector('#btn-double-next')
+const doublePreviousPage = document.querySelector('#btn-double-previous')
 
 nextPage.onclick = () => {
     currentPage += 1
@@ -53,3 +57,13 @@ previousPage.onclick = () => {
     if (currentPage) getInfo(`https://rickandmortyapi.com/api/character?page=${currentPage}`)
 }
 
+doubleNextPage.onclick = () => {
+    currentPage += 2
+    if (currentPage <= numberOfPages) getInfo(`https://rickandmortyapi.com/api/character?page=${currentPage}`)
+}
+
+doublePreviousPage.onclick = () => {
+    currentPage -= 2
+    if (currentPage) getInfo(`https://rickandmortyapi.com/api/character?page=${currentPage}`)
+
+}
